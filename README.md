aa6012849e6c710cc2f664ac3c1387350d311dfb
Phaninder <pasupulaphani@gmail.com>
2015-06-27 21:07:24 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,75 @@
+# Passport-TripCase
+
+[Passport](http://passportjs.org/) strategy for authenticating with [TripCase](https://www.tripcase.com/)
+using the OAuth 2.0 API.
+
+This module lets you authenticate using TripCase in your Node.js applications.
+By plugging into Passport, TripCase authentication can be easily and
+unobtrusively integrated into any application or framework that supports
+[Connect](http://www.senchalabs.org/connect/)-style middleware, including
+[Express](http://expressjs.com/).
+
+## Install
+
+    $ npm install passport-tripcase
+
+## Usage
+
+#### Configure Strategy
+
+The TripCase authentication strategy authenticates users using a TripCase account
+and OAuth 2.0 tokens.  The strategy requires a `verify` callback, which accepts
+these credentials and calls `done` providing a user, as well as `options`
+specifying a client ID, client secret, and callback URL.
+
+    passport.use(new TripCaseStrategy({
+        clientID: TRIPCASE_CLIENT_ID,
+        clientSecret: TRIPCASE_CLIENT_SECRET,
+        callbackURL: "http://127.0.0.1:3000/auth/tripcase/callback"
+      },
+      function(accessToken, refreshToken, profile, done) {
+        User.findOrCreate({ tripcaseId: profile.id }, function (err, user) {
+          return done(err, user);
+        });
+      }
+    ));
+
+#### Authenticate Requests
+
+Use `passport.authenticate()`, specifying the `'tripcase'` strategy, to
+authenticate requests.
+
+For example, as route middleware in an [Express](http://expressjs.com/)
+application:
+
+    app.get('/auth/tripcase',
+      passport.authenticate('tripcase'));
+
+    app.get('/auth/tripcase/callback', 
+      passport.authenticate('tripcase', { failureRedirect: '/login' }),
+      function(req, res) {
+        // Successful authentication, redirect home.
+        res.redirect('/');
+      });
+
+## Examples
+
+For a complete, working example, refer to the [login example](https://github.com/pasupulaphani/passport-tripcase/tree/master/examples/login).
+
+## Tests
+
+    $ npm install --dev
+    $ make test
+
+[![Build Status](https://secure.travis-ci.org/pasupulaphani/passport-tripcase.png)](http://travis-ci.org/pasupulaphani/passport-tripcase)
+
+## Credits
+
+  - [pasupulaphani](http://github.com/pasupulaphani)
+
+## License
+
+[The MIT License](http://opensource.org/licenses/MIT)
+
+Copyright (c) 2011-2013 Phaninder Pasupula <[http://phaninder.com/](http://phaninder.com/)>
+
---
+++ examples/login/app.js
@@ -0,0 +1,125 @@
+var express = require('express')
+  , passport = require('passport')
+  , util = require('util')
+  , TripCaseStrategy = require('passport-tripcase').Strategy;
+
+var TRIPCASE_CLIENT_ID = "276377b4760427c1c2f89e8c876e62fd79c5490ba3594a24aa47cf65246c7f25"
+var TRIPCASE_CLIENT_SECRET = "3ee390aedba147ed11cb545425b1cd752f3acc6c110b81095929f1aef0855424";
+
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.  However, since this example does not
+//   have a database of user records, the complete TripCase profile is serialized
+//   and deserialized.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+
+// Use the TripCaseStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken, refreshToken, and tripcase
+//   profile), and invoke a callback with a user object.
+passport.use(new TripCaseStrategy({
+    clientID: TRIPCASE_CLIENT_ID,
+    clientSecret: TRIPCASE_CLIENT_SECRET,
+    callbackURL: "https://airmax.herokuapp.com/auth/tripcase/callback",
+    authorizationURL: 'https://dharma.tripcase.com/client_api_permissions',
+    tokenURL: 'https://dharma.tripcase.com/developer_api/tokens',
+    userProfileURL: 'https://dharma.tripcase.com/developer_api/users',
+    userAgent: 'airmax-api'
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+      
+      // To keep the example simple, the user's TripCase profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the TripCase account with a user record in your database,
+      // and return that user instead.
+      return done(null, profile);
+    });
+  }
+));
+
+
+
+
+var app = express();
+
+// configure Express
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.logger());
+app.use(express.cookieParser());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.session({ secret: 'keyboard cat' }));
+// Initialize Passport!  Also use passport.session() middleware, to support
+// persistent login sessions (recommended).
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(app.router);
+app.use(express.static(__dirname + '/public'));
+
+
+
+app.get('/', function(req, res){
+  res.render('index', { user: req.user });
+});
+
+app.get('/account', ensureAuthenticated, function(req, res){
+  res.render('account', { user: req.user });
+});
+
+app.get('/login', function(req, res){
+  res.render('login', { user: req.user });
+});
+
+// GET /auth/tripcase
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in tripcase authentication will involve redirecting
+//   the user to tripcase.com.  After authorization, tripcase wii redirect the user
+//   back to this application at /auth/tripcase/callback
+app.get('/auth/tripcase',
+  passport.authenticate('tripcase'),
+  function(req, res){
+    // The request will be redirected to tripcase for authentication, so this
+    // function will not be called.
+  });
+
+// GET /auth/tripcase/callback
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+app.get('/auth/tripcase/callback', 
+  passport.authenticate('tripcase', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+app.listen(3000);
+
+
+// Simple route middleware to ensure user is authenticated.
+//   Use this route middleware on any resource that needs to be protected.  If
+//   the request is authenticated (typically via a persistent login session),
+//   the request will proceed.  Otherwise, the user will be redirected to the
+//   login page.
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login')
+}
